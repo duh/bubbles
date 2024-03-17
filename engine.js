6c800e7a6e173f94690a20f01ec72bbdc573411a
icy ❄ <62608915+duh@users.noreply.github.com>
2024-03-17 13:13:02 +0000

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,42 @@
+const page = document.getElementById("main");
+
+const randBetween = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+const randColour = function()
+{
+    return `rgb(${randBetween(0,255)},${randBetween(0,255)},${randBetween(0,255)})`
+}
+
+const createBubble = function(e)
+{
+    const bubble = document.createElement("div");
+    bubble.setAttribute("class", "bubble");
+    bubble.style.backgroundColor = randColour();
+    bubble.style.left = `${e.clientX - 25}px`
+    bubble.style.top = `${e.clientY - 25}px`
+    page.append(bubble);
+
+    const pop = bubble.animate(
+        [
+            {
+                opacity: 1
+            },
+            {
+                opacity: 0
+            }
+        ],
+        {
+            duration: 2000,
+            easing: "linear"
+        }
+    )
+
+    pop.onfinish = function()
+    {
+        bubble.remove();
+    }
+}
+
+page.addEventListener("mousemove", createBubble)
