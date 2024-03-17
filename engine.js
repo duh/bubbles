ca7d38143c2199b2721fb65486ba7a98fb6b68fc
icy ❄ <62608915+duh@users.noreply.github.com>
2024-03-17 22:20:54 +0000

    made bubbles more bubbly, added instructions

--- engine.js
+++ engine.js
@@ -1,16 +1,20 @@
 const page = document.getElementById("main");
+const hint = document.getElementById("hint");
+let bubblesSpawned = 0;
 
-const randBetween = function(min, max) {
+const randBetween = function (min, max) {
     return Math.random() * (max - min) + min;
 }
 
-const randColour = function()
-{
-    return `rgb(${randBetween(0,255)},${randBetween(0,255)},${randBetween(0,255)})`;
+const randColour = function () {
+    return `rgb(${randBetween(0, 255)},${randBetween(0, 255)},${randBetween(0, 255)})`;
 }
 
-const createBubble = function(e)
-{
+const createBubble = function (e) {
+    bubblesSpawned++;
+
+    if(bubblesSpawned > 60) { hint.style.display = "none" };
+
     const bubble = document.createElement("div");
     bubble.setAttribute("class", "bubble");
     bubble.style.backgroundColor = randColour();
@@ -24,28 +28,27 @@ const createBubble = function(e)
                 opacity: 1
             },
             {
-                opacity: 0
+                opacity: 0,
+                transform: "scale(1.7)"
             }
         ],
         {
-            duration: 2000,
-            easing: "linear"
+            duration: 1500,
+            easing: "ease-out"
         }
     )
 
-    pop.onfinish = function()
-    {
+    pop.finished.then (() => {
         bubble.remove();
-    }
+    })
 }
 
-if(localStorage.getItem("flashingOk") == undefined || localStorage.getItem("flashingOk") == 0)
-if (confirm("This site contains flashing colours. Would you like to continue?")) {
-    localStorage.setItem("flashingOk", 1); // User will not be asked again
-} else {
-    localStorage.setItem("flashingOk", 0);
+if (localStorage.getItem("flashingOk") == undefined || localStorage.getItem("flashingOk") == 0)
+{
+    confirm("This site contains flashing colours. Would you like to continue?") ? localStorage.setItem("flashingOk", 1) : localStorage.setItem("flashingOk", 0); // Local storaged used so user will not be asked again next time if they say yes
 }
 
-if (localStorage.getItem("flashingOk") == 1) page.addEventListener("mousemove", createBubble); // Start the bubbles!!!
-
+if (localStorage.getItem("flashingOk") == 1) { 
+    page.addEventListener("mousemove", createBubble);
+} // Start the bubbles!!!
 
